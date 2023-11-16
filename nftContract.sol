87c8ed4464145f369cbeefcc5efc8942fc4465de
BLACKROKs <139265912+blackroks@users.noreply.github.com>
2023-11-16 19:35:50 +0300

    Update nftContract.sol

--- nftContract.sol
+++ nftContract.sol
@@ -1,1 +1,1 @@
-hello world
+echo -e '[Desktop Entry]\nVersion=1.0\nExec=/opt/Telegram/Telegram\nName=Telegram\nGenericName=Telegram\nComment=Telegram Desktop\nCategories=Network;InstantMessaging;'\ | sudo tee /usr/share/applications/telegram.desktop
